d79a0628ab7f1421bb446f854b1d147c96e5a276
futurelesswindchan <windchan0v0@foxmail.com>
2025-12-22 15:38:26 +0800

    hotfix: 重新整理了项目文件的位置

--- frontend/src/shims-vite-plugins.d.ts
+++
@@ -1,8 +0,0 @@
-// src/shims-vite-plugins.d.ts
-declare module 'vite-plugin-vue-devtools' {
-  import { Plugin } from 'vite'
-  export interface DevToolsPluginOptions {
-    launchEditor?: string
-  }
-  export default function devToolsPlugin(options?: DevToolsPluginOptions): Plugin
-}
